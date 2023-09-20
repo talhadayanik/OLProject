908e8612b7f820cbed86b1a34caa01ec5872e52c
piyonist <htalha@hotmail.com>
2023-09-20 22:05:30 +0300

    added map, minor bug fixes
    added map, minor bug fixes

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -3,17 +3,46 @@ import { Route, Routes } from 'react-router-dom';
 import './custom.css';
 import './App.css';
 import ParcelItem from './components/ParcelItem';
-import {
-  interaction, layer, custom, control, //name spaces
-  Interactions, Overlays, Controls,     //group
-  Map, Layers, Overlay, Util    //objects
-} from "react-openlayers";
+import Map from "./OpenLayers/Map";
+import { Layers, TileLayer, VectorLayer } from "./OpenLayers/Layers";
+import { Style, Icon } from "ol/style";
+import Feature from "ol/Feature";
+import Point from "ol/geom/Point";
+import { osm, vector } from "./OpenLayers/Source";
+import { fromLonLat, get } from "ol/proj";
+import GeoJSON from "ol/format/GeoJSON";
+import { Controls, FullScreenControl } from "./OpenLayers/Controls";
+import FeatureStyles from "./OpenLayers/Features/Styles";
+import mapConfig from "./map_config.json";
+import Draw from 'ol/interaction/Draw.js';
+import View from 'ol/View.js';
+
+function addMarkers(lonLatArray) {
+  var iconStyle = new Style({
+    image: new Icon({
+      anchorXUnits: "fraction",
+      anchorYUnits: "pixels",
+      src: mapConfig.markerImage32,
+    }),
+  });
+  let features = lonLatArray.map((item) => {
+    let feature = new Feature({
+      geometry: new Point(fromLonLat(item)),
+    });
+    feature.setStyle(iconStyle);
+    return feature;
+  });
+  return features;
+}
 
 const App = () => {
 
   const BASE_URL = "https://localhost:7141";
 
   const [parcels, setParcels] = useState([]);
+  const [center, setCenter] = useState(mapConfig.center);
+  const [zoom, setZoom] = useState(9);
+  const [features, setFeatures] = useState();
 
   const getAllParcels = async () => {
     
@@ -36,13 +65,16 @@ const App = () => {
       <div className='app'>
         <h1>OLProject</h1>
       </div>
-      
-      <Map view={{center:[0, 0], zoom:2}}>
-        <Layers>
-          <layer.Tile></layer.Tile>
-        </Layers>
-      </Map>
 
+      <Map center={fromLonLat(center)} zoom={zoom}>
+        <Layers>
+        <TileLayer source={osm()} zIndex={0} />
+        </Layers>
+        <Controls>
+          <FullScreenControl />
+        </Controls>
+      </Map>
+      
       {parcels?.length > 0 ? (
         <div>
             {parcels.map((parcel) => (
--- ClientApp/src/components/ParcelItem.jsx
+++ ClientApp/src/components/ParcelItem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ParcelItem = ({parcel}) => {
     return(
         <div className='general'>
-            <h2>{parcel.province} {parcel.district} {parcel.neighborhood}</h2>
+            <h2>{parcel.district}, {parcel.neighborhood}, {parcel.province} </h2>
         </div>
     );
 }
